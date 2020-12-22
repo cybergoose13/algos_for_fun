d66f3b0ee9e9fe97b8afa025e044b65c3cd61607
BuildTools <cybergoose13@gmail.com>
2020-12-22 15:55:16 -0800

    still working on it =p

--- DIY_HASHER.py
+++ DIY_HASHER.py
@@ -2,19 +2,20 @@
 
 import math
 
-str_to_hash= "Hello"
+str_to_hash= input("Enter a string to hash:\n");
 
-def JCHA(mstr):
-    length= len(mstr);
+def JCHA(my_str):
+    my_str= my_str.replace(' ', '');
+    length= len(my_str);
     temp_array= [];
     jcha= '';
-    
-    for x in mstr:
-        temp_array.append(hex(int(math.pow(ord(x), length))));
+
+    for x in my_str:
+        temp_array.append(hex(int(math.pow((ord(x) + 200) , length))));
         
     for item in temp_array:
-        x= str(item).split('0x');
-        jcha += x[1];
+        jcha += item.replace('0x', '');
+        
     return jcha;
 
 print(JCHA(str_to_hash));
