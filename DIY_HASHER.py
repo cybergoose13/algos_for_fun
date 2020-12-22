f6d4ef3408fb7bfd07c4d8d05d7e7760a96f8df7
BuildTools <cybergoose13@gmail.com>
2020-12-22 15:22:31 -0800

    fixing hashing algo

--- DIY_HASHER.py
+++ DIY_HASHER.py
@@ -6,32 +6,15 @@ str_to_hash= "Hello"
 
 def JCHA(mstr):
     length= len(mstr);
-    print("Length of string: " + str(length));
-    # make and set string of each char's binary value.
-    binary_string= ('' + '\n'.join(format(ord(i), 'b') for i in mstr));
-    print("Binary String: " + binary_string);
-
-    # split the binary string into an array per char.
-    binary_array= binary_string.split('\n');
-    print("Binary Array: " + str(binary_array));
-
-    temp= 0;
     temp_array= [];
-    # loop through the binary string values
-    for c in binary_array:
-        # num= int(c) * length;
-        # num_array= str(num);
-
-        for n in c:
-            temp += int(n);
-
-        # temp_array.append(hex(temp));
+    jcha= '';
+    
+    for x in mstr:
+        temp_array.append(hex(int(math.pow(ord(x), length))));
         
-        print("temp is: " + str(temp));
-        temp_array.append( hex( int(math.pow(temp, length)) ));
-        temp=0;
-
-
-    return temp_array;
+    for item in temp_array:
+        x= str(item).split('0x');
+        jcha += x[1];
+    return jcha;
 
 print(JCHA(str_to_hash));
