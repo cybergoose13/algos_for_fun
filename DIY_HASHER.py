d39c5ca31bbb8275b177cc0fd3ebe611879ddac3
BuildTools <cybergoose13@gmail.com>
2020-12-22 23:19:23 -0800

    more hashing

--- DIY_HASHER.py
+++ DIY_HASHER.py
@@ -1,21 +1,36 @@
 # INCOMPLETE 
 
 import math
+import sys
 
 str_to_hash= input("Enter a string to hash:\n");
 
 def JCHA(my_str):
     my_str= my_str.replace(' ', '');
     length= len(my_str);
-    temp_array= [];
     jcha= '';
 
-    for x in my_str:
-        temp_array.append(hex(int(math.pow((ord(x) + 200) , length))));
-        
-    for item in temp_array:
-        jcha += item.replace('0x', '');
-        
+    for my_char in my_str:
+        temp= int(math.pow( (ord(my_char) + 200), length));
+        temp<< temp % length;
+        print(sys.getsizeof(temp));
+        jcha+= hex(temp).replace('0x', '');
+    
+    jcha= jcha.replace('0', '');
+    byte_size= sys.getsizeof(jcha);
+
+    if(byte_size != 128):
+        if(byte_size > 128):
+            while(byte_size != 128):
+                jcha= jcha[:-1];
+                byte_size= sys.getsizeof(jcha);
+        else:
+            counter= 0;
+            while(byte_size != 128):
+                jcha+= jcha[counter];
+                counter+= 1;
+                byte_size= sys.getsizeof(jcha);
+    print(sys.getsizeof(jcha));
     return jcha;
 
 print(JCHA(str_to_hash));
