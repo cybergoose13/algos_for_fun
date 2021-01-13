4d726505835ac598433dfe4efe26ec2b94f899d8
BuildTools <cybergoose13@gmail.com>
2021-01-12 20:07:17 -0800

    Single Linked List in python

---
+++ LinkedList.py
@@ -0,0 +1,45 @@
+'''
+    Practice with linked lists
+'''
+
+class Node:
+    def __init__(self, data=None):
+        self.data= data
+        self.next= None
+
+class LinkedList:
+    def __init__(self):
+        self.head= None
+
+    def getTail(self):
+        pass
+
+    def size(self):
+        counter= 0
+        value= self.head
+        while value:
+            counter += 1
+            value= value.next
+        return counter
+
+    def printList(self):
+        value= self.head
+        while value:
+            print(value.data)
+            value= value.next
+
+
+if __name__ == "__main__":
+    list1= LinkedList()
+    list1.head= Node('One')
+    second= Node('second')
+    third= Node('Third')
+    fourth= Node('4th')
+    list1.head.next= second
+    second.next= third
+    third.next= fourth
+
+    list2= LinkedList()
+
+    print(list1.size())
+    print(list2.size())
