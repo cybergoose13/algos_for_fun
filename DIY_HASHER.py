96ce235e7b29a8a8d257034f79fbc6413b8b8a78
unknown <cybergoose13@gmail.com>
2021-03-08 20:54:14 -0800

    fix random semicolons

--- DIY_HASHER.py
+++ DIY_HASHER.py
@@ -4,34 +4,34 @@
 import math
 import sys
 
-str_to_hash= input("Enter a string to hash:\n");
+str_to_hash= input("Enter a string to hash:\n")
 
 def JCHA(my_str):
-    my_str= my_str.replace(' ', '');
-    length= len(my_str);
-    jcha= '';
+    my_str= my_str.replace(' ', '')
+    length= len(my_str)
+    jcha= ''
 
     for my_char in my_str:
-        temp= int(math.pow( (ord(my_char) + 200), length));
-        temp<< temp % length;
-        print(sys.getsizeof(temp));
-        jcha+= hex(temp).replace('0x', '');
+        temp= int(math.pow( (ord(my_char) + 200), length))
+        temp<< temp % length
+        print(sys.getsizeof(temp))
+        jcha+= hex(temp).replace('0x', '')
     
-    jcha= jcha.replace('0', '');
-    byte_size= sys.getsizeof(jcha);
+    jcha= jcha.replace('0', '')
+    byte_size= sys.getsizeof(jcha)
 
     if(byte_size != 128):
         if(byte_size > 128):
             while(byte_size != 128):
-                jcha= jcha[:-1];
-                byte_size= sys.getsizeof(jcha);
+                jcha= jcha[:-1]
+                byte_size= sys.getsizeof(jcha)
         else:
-            counter= 0;
+            counter= 0
             while(byte_size != 128):
-                jcha+= jcha[counter];
-                counter+= 1;
-                byte_size= sys.getsizeof(jcha);
-    print(sys.getsizeof(jcha));
-    return jcha;
+                jcha+= jcha[counter]
+                counter+= 1
+                byte_size= sys.getsizeof(jcha)
+    print(sys.getsizeof(jcha))
+    return jcha
 
-print(JCHA(str_to_hash));
+print(JCHA(str_to_hash))
--- JCHA.py
+++ JCHA.py
@@ -10,47 +10,47 @@ class JCHA:
     Intended for practicing and understand simple encryption methods.
     """
 
-    text_length= 0;
+    text_length= 0
     def __init__(self) -> None:
-        super().__init__();
+        super().__init__()
 
     def hash_str(self, clear_text):
-        text_length= len(clear_text);
-        clear_text= clear_text.replace(' ', '');
-        text_hashed= '';
+        text_length= len(clear_text)
+        clear_text= clear_text.replace(' ', '')
+        text_hashed= ''
 
         for text_char in clear_text:
             x= (ord(text_char)**2)
-            unicode_value= (ord(text_char) + x);
-            temp= int(math.pow( unicode_value, text_length));
-            temp<< temp % text_length;
-            text_hashed+= hex(temp).replace('0x', '');
+            unicode_value= (ord(text_char) + x)
+            temp= int(math.pow( unicode_value, text_length))
+            temp<< temp % text_length
+            text_hashed+= hex(temp).replace('0x', '')
 
-        text_hashed= text_hashed.replace('0', '');
-        byte_size= sys.getsizeof(text_hashed);
-        return self.setBytes(text_hashed, byte_size);
+        text_hashed= text_hashed.replace('0', '')
+        byte_size= sys.getsizeof(text_hashed)
+        return self.setBytes(text_hashed, byte_size)
 
     def setBytes(self, hash, byte_size):
         
         if(byte_size != 128):
             if(byte_size > 128):
                 while(byte_size != 128):
-                    hash= hash[:-1];
-                    byte_size= sys.getsizeof(hash);
+                    hash= hash[:-1]
+                    byte_size= sys.getsizeof(hash)
             else:
                 while(byte_size != 128):
-                    temp= sys.getsizeof(hash) % len(hash);
-                    hash+= hex(sys.getsizeof(hash) + temp).replace('0x', '');
-                    byte_size= sys.getsizeof(hash);
-        return hash;
+                    temp= sys.getsizeof(hash) % len(hash)
+                    hash+= hex(sys.getsizeof(hash) + temp).replace('0x', '')
+                    byte_size= sys.getsizeof(hash)
+        return hash
 
 
     def compare(self, clear_str, hashed_str):
-        return (self.hash_str(clear_str) == hashed_str);
+        return (self.hash_str(clear_str) == hashed_str)
 
 
-text_in= input("Enter text to hash:\n");
-jcha= JCHA();
-hashed_text= jcha.hash_str(text_in);
-print(jcha.compare(input("Compare text:\n"), hashed_text));
-print(hashed_text);
+text_in= input("Enter text to hash:\n")
+jcha= JCHA()
+hashed_text= jcha.hash_str(text_in)
+print(jcha.compare(input("Compare text:\n"), hashed_text))
+print(hashed_text)
